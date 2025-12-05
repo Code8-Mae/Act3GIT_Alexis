042d13b3cdf636db8aa330b168cb3490f7fa7954
Alexis Esteban Martín <aemgame4008@gmail.com>
2025-12-05 10:35:07 +0100

    ahora se muestran 30 numeros

--- src/proyecto/proyecto.java
+++ src/proyecto/proyecto.java
@@ -4,7 +4,7 @@ public class proyecto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int f = 0; f < 20; f++) {
+		for(int f = 0; f < 30; f++) {
 			int num = (int) (Math.random() * 10 ) + 1;
 			System.out.println("Número " + (f + 1) + ": " + num);
 		}
